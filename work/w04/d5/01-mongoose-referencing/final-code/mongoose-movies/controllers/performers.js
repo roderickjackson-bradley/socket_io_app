997748bb10f690318136f9a84c86f41aa46234b6
jim-clark <email@jim-clark.com>
2019-02-15 12:14:13 -0800

    Fix day off by one issue on mongoose-referencing

--- work/w04/d5/01-mongoose-referencing/final-code/mongoose-movies/controllers/performers.js
+++ work/w04/d5/01-mongoose-referencing/final-code/mongoose-movies/controllers/performers.js
@@ -8,22 +8,27 @@ module.exports = {
 };
 
 function addToCast(req, res) {
-  Movie.findById(req.params.id, function(err, movie) {
+  Movie.findById(req.params.id, function (err, movie) {
     movie.cast.push(req.body.performerId);
-    movie.save(function(err) {
+    movie.save(function (err) {
       res.redirect(`/movies/${movie._id}`);
     });
   });
 }
 
 function create(req, res) {
-  Performer.create(req.body, function(err, performer) {
+  // Need to "fix" date formatting to prevent day off by 1
+  // This is due to the <input type="date"> returning the date
+  // string in this format:  "YYYY-MM-DD"
+  var s = req.body.born;
+  req.body.born = `${s.substr(5, 2)}-${s.substr(8, 2)}-${s.substr(0, 4)}`;
+  Performer.create(req.body, function (err, performer) {
     res.redirect('/performers/new');
   });
 }
 
 function newPerformer(req, res) {
-  Performer.find({}, function(err, performers) {
+  Performer.find({}, function (err, performers) {
     res.render('performers/new', {
       title: 'Add Performer',
       performers
